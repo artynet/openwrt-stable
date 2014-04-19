a08409fa3dc15e57ba1de6464f76a3d401450a0a
Mimmo <mimmo@linino.org>
2014-04-19 12:23:43 +0200

    Partial commit chiwawa board

--- target/linux/ar71xx/generic/profiles/linino.mk
+++ target/linux/ar71xx/generic/profiles/linino.mk
@@ -51,3 +51,16 @@ define Profile/LININO_FREEDOG/Description
 endef
 
 $(eval $(call Profile,LININO_FREEDOG))
+
+define Profile/LININO_CHIWAWA
+        NAME:=Linino Chiwawa
+        PACKAGES:=kmod-usb-core kmod-usb2 luci-webpanel-linino
+endef
+
+define Profile/LININO_CHIWAWA/Description
+        Package set optimized for the Linino Chiwawa based on
+        Atheros AR9331.
+endef
+
+$(eval $(call Profile,LININO_CHIWAWA))
+
